2136adda0949809e797a91febc4fe748e47b48d7
endorphin82 <endorphin82@gmail.com>
2018-10-08 20:50:59 +0300

    10.07.AddClientToFirestore

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import store from './store';
 
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from './components/layout/Dashboard';
+import AddClient from "./components/clients/AddClient";
 
 import './App.css';
 
@@ -18,7 +19,8 @@ class App extends Component {
             <AppNavbar/>
             <div className="container">
               <Switch>
-                <Route exax path="/" component={Dashboard} />
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/client/add" component={AddClient} />
               </Switch>
             </div>
           </div>
